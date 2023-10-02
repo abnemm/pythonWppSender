3695bc10b500049f964dd98967a6576c3e4aef3f
Renan-Bahia <renan.bahia@stellantis.com>
2023-10-02 14:51:15 -0300

    inicio api

--- componenteMsg.py
+++ componenteMsg.py
@@ -1,5 +1,7 @@
 import time
 from datetime import datetime, timedelta, date
+from fastapi import FastAPI
+# from flask import Flask, request
 # from datetime import datetime, timedelta
 # import pandas as pd
 
@@ -7,6 +9,9 @@ import sys, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+app = FastAPI()
+
 
 class componenteMsg:
 
@@ -46,25 +51,72 @@ class componenteMsg:
                 element.send_keys(destinatario)
                 time.sleep(0.1)
                 element.send_keys(Keys.ENTER)
-                print(mensagem)
-
-
-
-
-
-            # print(componenteMsg.navegador.find)
+                element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+                time.sleep(0.1)
+                for var in mensagem:
+                    element.send_keys(var)
+                    element.send_keys(Keys.SHIFT+Keys.ENTER)
+                    print(var)
+                element.send_keys(Keys.ENTER)
+            
+            if(imagem!=0):
+                print("asd")
+                element = navegador.find_element(by=By.XPATH,value='//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')
+                element.send_keys(destinatario)
+                time.sleep(0.1)
+                element.send_keys(Keys.ENTER)
+                icone_anexo = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div')
+                icone_anexo.click()
+                navegador.implicitly_wait(5)
+                opcao_imagem = navegador.find_element(by=By.XPATH,value='//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+                opcao_imagem.send_keys(imagem)
+                time.sleep(5)
+                element = navegador.find_element(by=By.XPATH,value='//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/p')
+                for var in mensagem:
+                    element.send_keys(var)
+                    element.send_keys(Keys.SHIFT+Keys.ENTER)
+                    print(var)
+                element.send_keys(Keys.ENTER)
+                time.sleep(1)
+                element.send_keys(Keys.ENTER)
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             open("errorlog.txt","a").write(f"\n{datetime.now()} - Erro ao criar pagina - {exc_type} - {e} - {fname} {exc_tb.tb_lineno}")
-        
+
+class mensagem():
+    texto = [str]
+    dest = str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.put("/asd/{mensagem}")
+def update_item(mensagem: int):
+    return {"item_name": item.name, "item_id": item_id}
+
+    # # objeto = componenteMsg
+    # # navegador = objeto.criaPagWeb()
+
+    # @app.route("/",methods=['GET','POST'])
+    # def response(self):
+    #     dest = request.form('dest')
+    #     msg = request.form('msg')
+    #     open("errorlog.txt","a").write(f"\n{datetime.now()} - NOME={dest} MSG={msg}")
+    #     open("errorlog.txt","a").write(f"\n{datetime.now()} - NOME={dest} MSG={msg}")
+    # # objeto.enviaMensagem(destinatario=dest, mensagem=[msg])
 
 
 
 
+# objeto = componenteMsg
+# navegador = objeto.criaPagWeb()
 
-objeto = componenteMsg
-navegador = objeto.criaPagWeb()
-time.sleep(6)
-objeto.enviaMensagem(destinatario="abne", mensagem="asdasdasdasd")
+# objeto = componenteMsg
+# navegador = objeto.criaPagWeb()
+# time.sleep(6)
+# objeto.enviaMensagem(destinatario="abne", mensagem="asdasdasdasd")
+
--- old/enviaAlerta.py
+++ old/enviaAlerta.py
@@ -13,11 +13,16 @@ from datetime import datetime, timedelta
 import pandas as pd
 global con
 global cursor
+import simon
+from simon.accounts.pages import LoginPage
+from simon.chat.pages import ChatPage
+from simon.chats.pages import PanePage
+from simon.header.pages import HeaderPage
 
 def criaSql():
 
     global con, cursor
-    con = mysql.connector.connect(host='localhost', user='asd', password='asd@1234',database='chatbot')
+    con = mysql.connector.connect(host='172.20.233.63', user='asd', password='asd@1234',database='chatbot')
     if con.is_connected():
         db_info = con.get_server_info()
         cursor = con.cursor()
@@ -37,7 +42,7 @@ def fazQuery(query):
 def setup():
     global navegador
     # navegador = webdriver.Chrome(ChromeDriverManager().install())
-    navegador = webdriver.Chrome(r'C:\Users\F54569C\Desktop\Abne\chromedriver.exe')
+    navegador = webdriver.Chrome(r'F:\Alerta\chromedriver.exe')
     navegador.get(r'https://web.whatsapp.com/')
     time.sleep(1)
     print("Aguarda ler qrcode com o celular")
@@ -48,15 +53,14 @@ def setup():
 
 def enviaMensagemWpp(sensor, funcionario):
     mensagem = []
-    mensagem.append(f'Ola {funcionario[1]}!')
+    mensagem.append(f'Olá {funcionario[1]}!')
     mensagem.append(f'Houve um alerta no sensor {sensor[1]} localizado em {sensor[3]}')
     mensagem.append(f'')
-    mensagem.append(f'As dicas para resolver o problema são:')
-    mensagem.append(f'')
-    mensagem.append(f'1-{sensor[4]}')
-    mensagem.append(f'2-{sensor[5]}')
-    mensagem.append(f'3-{sensor[6]}')
-
+    # mensagem.append(f'As dicas para resolver o problema são:')
+    # mensagem.append(f'')
+    # mensagem.append(f'1-{sensor[4]}')
+    # mensagem.append(f'2-{sensor[5]}')
+    # mensagem.append(f'3-{sensor[6]}')
 
     element = navegador.find_element(by=By.XPATH,value='//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')
 
@@ -68,9 +72,11 @@ def enviaMensagemWpp(sensor, funcionario):
     icone_anexo.click()
     navegador.implicitly_wait(5)
     opcao_imagem = navegador.find_element(by=By.XPATH,value='//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-    opcao_imagem.send_keys(sensor[7])
+    # opcao_imagem.send_keys(sensor[7])
+    opcao_imagem.send_keys(sensor[8])
     time.sleep(10)
     element = navegador.find_element(by=By.XPATH,value='//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/p')
+    
     for linha in mensagem:
         element.send_keys(linha)
         element.send_keys(Keys.SHIFT+Keys.ENTER)
@@ -78,6 +84,134 @@ def enviaMensagemWpp(sensor, funcionario):
     # botao_enviar = navegador.find_element(by=By.XPATH,value='//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span')
     # botao_enviar.click()
     element.send_keys(Keys.ENTER)
+
+    fazQuery(f'select * from correcoes where fk_codigoSensor = {sensor[0]} order by ultimoOcorrido desc LIMIT 1')
+    ultimo = cursor.fetchone()
+    fazQuery('Select Max( quantidadeOcorridos ) As Valor From correcoes')
+    maior = cursor.fetchone()
+    fazQuery(f'select * from correcoes where fk_codigoSensor = {sensor[0]} and quantidadeOcorridos = {maior[0]} and id != {ultimo[0]} ')
+    maiores = cursor.fetchall()
+    exclusoes = (f' id != {ultimo[0]}')
+    for var in maiores:
+        exclusoes = (f'{exclusoes} and id != {var[0]} ')
+    fazQuery(f'SELECT * FROM `correcoes` where {exclusoes}')
+    restante = cursor.fetchall()
+
+    element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+
+    element.send_keys("A ultima falha foi resolvida da seguinte forma:")
+    element.send_keys(Keys.SHIFT+Keys.ENTER)
+    time.sleep(0.1)
+    element.send_keys(ultimo[2])
+    element.send_keys(Keys.ENTER)
+
+    element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+    element.send_keys("A resolução funcionou?")
+    element.send_keys(Keys.ENTER)
+    resposta = ultima_msg(navegador)
+    print(resposta)
+    print(type(resposta))
+    if ((resposta == 'sim') or
+        (resposta == 'SIM') or
+        (resposta == 'Sim')
+        ):
+            print ('sim recente')
+            agora = datetime.now()
+            element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+            element.send_keys("Otimo!")
+            element.send_keys(Keys.ENTER)
+            fazQuery(f'UPDATE correcoes SET quantidadeOcorridos = quantidadeOcorridos +1 and ultimoOcorrido = "{agora}" WHERE id = {ultimo[0]};')
+            return
+
+    elif((resposta == 'nao') or
+          (resposta == 'NAO') or
+          (resposta == 'Nao') or
+          (resposta == 'NÃO') or
+          (resposta == 'não') or
+          (resposta == 'Não')) :
+    
+    
+            print ('Não recente')
+            
+            for lista in maiores:
+
+                element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                element.send_keys(Keys.SHIFT+Keys.ENTER)
+                time.sleep(0.1)
+                element.send_keys(lista[2])
+                element.send_keys(Keys.ENTER)
+
+                element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                element.send_keys("A resolução funcionou?")
+                element.send_keys(Keys.ENTER)
+                resposta = ultima_msg(navegador)
+
+                if ((resposta == 'sim') or
+                    (resposta == 'SIM') or
+                    (resposta == 'Sim')
+                ):
+                        print ('sim maior')
+                        agora = datetime.now()
+                        element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                        element.send_keys("Otimo!")
+                        element.send_keys(Keys.ENTER)
+                        fazQuery(f'UPDATE correcoes SET quantidadeOcorridos = quantidadeOcorridos +1 and ultimoOcorrido = "{agora}" WHERE id = {lista[0]};')
+                        return
+
+                elif((resposta == 'nao') or
+                    (resposta == 'NAO') or
+                    (resposta == 'Nao') or
+                    (resposta == 'NÃO') or
+                    (resposta == 'não') or
+                    (resposta == 'Não')) :
+                        print ('Não maior')
+
+                        for item in restante:
+
+                            element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                            element.send_keys(Keys.SHIFT+Keys.ENTER)
+                            time.sleep(0.1)
+                            element.send_keys(lista[2])
+                            element.send_keys(Keys.ENTER)
+
+                            element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                            element.send_keys("A resolução funcionou?")
+                            element.send_keys(Keys.ENTER)
+                            resposta = ultima_msg(navegador)
+
+                            if ((resposta == 'sim') or
+                                (resposta == 'SIM') or
+                                (resposta == 'Sim')
+                            ):
+                                    print ('sim resto')
+                                    agora = datetime.now()
+                                    element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                                    element.send_keys("Otimo!")
+                                    element.send_keys(Keys.ENTER)
+                                    fazQuery(f'UPDATE correcoes SET quantidadeOcorridos = quantidadeOcorridos +1 and ultimoOcorrido = "{agora}" WHERE id = {item[0]};')
+                                    return
+
+                            elif((resposta == 'nao') or
+                                (resposta == 'NAO') or
+                                (resposta == 'Nao') or
+                                (resposta == 'NÃO') or
+                                (resposta == 'não') or
+                                (resposta == 'Não')) :
+                                    print ('Não resto')
+
+                                    element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                                    element.send_keys("Então Como foi feita a resolução?")
+                                    element.send_keys(Keys.ENTER)
+                                    resposta = ultima_msg(navegador)
+
+                                    
+                                    element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]')
+                                    element.send_keys("Obrigado pela cooperação!")
+                                    element.send_keys(Keys.ENTER)
+                                    agora = datetime.now()
+                                    fazQuery(f'insert into correcoes (fk_codigoSensor, correcao, ultimoOcorrido, quantidadeOcorridos) values({sensor[0]}, "{resposta}" ,"{agora}",1);')
+
+
 
 def turnoAtual():
     dataHora= datetime.now()
@@ -88,22 +222,40 @@ def turnoAtual():
     else:
         return 2
 
+def ultima_msg(navegador):
+        
+        """ Captura a ultima mensagem da conversa """
+        try:
+            post = navegador.find_elements(by = By.CLASS_NAME , value="_21Ahp")
+            ultimo = len(post) - 1
+            print(f'id ultimo {ultimo}')
+            mensagemParametro = post[ultimo].find_element_by_css_selector("span.selectable-text").text
+            mensagemFinal = post[ultimo].find_element_by_css_selector("span.selectable-text").text
+            while(mensagemParametro == mensagemFinal):
+                # print(f'parametro: {mensagemParametro}')
+                # print(f'Final : {mensagemFinal}')
+                post = navegador.find_elements(by = By.CLASS_NAME , value="_21Ahp")
+                ultimo = len(post) - 1
+                mensagemFinal = post[ultimo].find_element_by_css_selector("span.selectable-text").text             
+            return mensagemFinal
+        except Exception as e:
+            print("Erro ao ler msg, tentando novamente!")
 
 def buscaSensoresPlc():
         
         fazQuery("select estado from alerta where id = 1")
         estadoDoAlerta = cursor.fetchone()
-        if estadoDoAlerta[0] == 1:
-            fazQuery("update alerta set estado = 0 where id = 1")
-            fazQuery("select * from sensores where bitValidacao = 1")
-            sensores = cursor.fetchall()
-            for sensor in sensores:
-                cursor.execute(f"select * from funcionarios where UTE = {sensor[2]} and turno = {turnoAtual()}") 
-                funcionarios = cursor.fetchall()
-                for func in funcionarios:
-                    enviaMensagemWpp(sensor = sensor, funcionario=func)
-                fazQuery(f"update sensores set bitValidacao = 0 where codigo = {sensor[0]}")
-                time.sleep(60)  
+        # if estadoDoAlerta[0] == 1:
+        fazQuery("update alerta set estado = 0 where id = 1")
+        fazQuery("select * from sensores where bitValidacao = 1")
+        sensores = cursor.fetchall()
+        for sensor in sensores:
+            cursor.execute(f"select * from funcionarios where UTE = {sensor[2]} and turno = {turnoAtual()}") 
+            funcionarios = cursor.fetchall()
+            for func in funcionarios:
+                enviaMensagemWpp(sensor = sensor, funcionario=func)
+            # fazQuery(f"update sensores set bitValidacao = 0 where codigo = {sensor[0]}")
+            time.sleep(60)  
     
 
 

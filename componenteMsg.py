68969c4d946c11f7dd55d98807d87e084f08e8ce
abnemm <abnemartins15@gmail.com>
2023-10-05 14:45:20 -0300

    att

--- componenteMsg.py
+++ componenteMsg.py
@@ -1,16 +1,20 @@
 import time
 from datetime import datetime, timedelta, date
-from fastapi import FastAPI
-# from flask import Flask, request
 # from datetime import datetime, timedelta
 # import pandas as pd
 
+from pydantic import BaseModel
 import sys, os
+from fastapi import  Request, FastAPI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-app = FastAPI()
+app= FastAPI()
+listaMsg= []
+# listaMsg.append( {"nome": "abne" , "msg" : ["sdafd","asdfaf","adsfasfd"] } )
+# listaMsg.append( {"nome": "julio", "msg" : ["aaaaaaa","asdddas","adsfasfd"], "img" : "imggggg"})
+# listaMsg.append( {"nome": "carlo", "msg" : ["asdsadsdsa","fffffffff","apppppp"]})
 
 
 class componenteMsg:
@@ -21,6 +25,15 @@ class componenteMsg:
         objeto= self
         # self.navegador = self.criaPagWeb()
         navegador = self.criaPagWeb()
+
+    @app.get('/')
+    async def aa():
+        return {"message": "Hello World"}
+
+    @app.post('/post/')
+    async def criaMsg(msg:str, dest:str):
+        listaMsg.append({"nome": dest, "msg": msg})
+    # open("errorlog.txt","a").write(f"\n{datetime.now()}  -  - {dest} ")
 
     def criaPagWeb():
         
@@ -45,78 +58,47 @@ class componenteMsg:
     def enviaMensagem(destinatario, mensagem, imagem=0):
 
         try:
-            
             if(imagem==0):
                 element = navegador.find_element(by=By.XPATH,value='//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')
                 element.send_keys(destinatario)
                 time.sleep(0.1)
                 element.send_keys(Keys.ENTER)
                 element = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-                time.sleep(0.1)
                 for var in mensagem:
                     element.send_keys(var)
                     element.send_keys(Keys.SHIFT+Keys.ENTER)
-                    print(var)
                 element.send_keys(Keys.ENTER)
-            
-            if(imagem!=0):
-                print("asd")
-                element = navegador.find_element(by=By.XPATH,value='//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')
-                element.send_keys(destinatario)
                 time.sleep(0.1)
-                element.send_keys(Keys.ENTER)
-                icone_anexo = navegador.find_element(by=By.XPATH,value='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div')
-                icone_anexo.click()
-                navegador.implicitly_wait(5)
-                opcao_imagem = navegador.find_element(by=By.XPATH,value='//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-                opcao_imagem.send_keys(imagem)
-                time.sleep(5)
-                element = navegador.find_element(by=By.XPATH,value='//*[@id="app"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/p')
-                for var in mensagem:
-                    element.send_keys(var)
-                    element.send_keys(Keys.SHIFT+Keys.ENTER)
-                    print(var)
-                element.send_keys(Keys.ENTER)
-                time.sleep(1)
-                element.send_keys(Keys.ENTER)
+
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             open("errorlog.txt","a").write(f"\n{datetime.now()} - Erro ao criar pagina - {exc_type} - {e} - {fname} {exc_tb.tb_lineno}")
 
-class mensagem():
-    texto = [str]
-    dest = str
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.put("/asd/{mensagem}")
-def update_item(mensagem: int):
-    return {"item_name": item.name, "item_id": item_id}
-
-    # # objeto = componenteMsg
-    # # navegador = objeto.criaPagWeb()
-
-    # @app.route("/",methods=['GET','POST'])
-    # def response(self):
-    #     dest = request.form('dest')
-    #     msg = request.form('msg')
-    #     open("errorlog.txt","a").write(f"\n{datetime.now()} - NOME={dest} MSG={msg}")
-    #     open("errorlog.txt","a").write(f"\n{datetime.now()} - NOME={dest} MSG={msg}")
-    # # objeto.enviaMensagem(destinatario=dest, mensagem=[msg])
+objeto = componenteMsg
+navegador = objeto.criaPagWeb()
 
 
 
 
-# objeto = componenteMsg
-# navegador = objeto.criaPagWeb()
 
-# objeto = componenteMsg
-# navegador = objeto.criaPagWeb()
-# time.sleep(6)
-# objeto.enviaMensagem(destinatario="abne", mensagem="asdasdasdasd")
+
+while(1):
+    try:
+        if(len(listaMsg) != 0): 
+            for var in listaMsg:
+                if "img" in var:
+                    objeto.enviaMensagem(destinatario=var['nome'],mensagem=var['msg'],imagem=var['img'])
+
+                else:
+                    objeto.enviaMensagem(destinatario=var['nome'], mensagem=var['msg'] )
+
+            listaMsg.clear()
+
+    except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            open("errorlog.txt","a").write(f"\n{datetime.now()} - Erro no envio  - {exc_type} - {e} - {fname} {exc_tb.tb_lineno}")
+        
 
